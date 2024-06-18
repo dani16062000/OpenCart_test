bd7ad9c82f711f01cdd341c01b08dffc7dc177d8
dpusa2012@gmail.com <16062000Dp>
2024-06-18 14:19:54 +0200

    setting up project

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,38 @@
 const { defineConfig } = require("cypress");
+const {allureCypress} = require("allure-cypress/reporter")
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  require('cypress-mochawesome-reporter/plugin')(on);
+  allureCypress(on, {
+    resultsDir: "./allure-results"
+  });
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
 
+  return config;
+}
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  reporter: 'cypresss-mochawesome-reporter',
+  reporterOptions:{
+    charts:true,
+    reportPageTitle:'report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
   },
+  e2e: {
+    watchForFileChanges: false,
+    baseUrl: 'http://opencart.abstracta.us',
+    setupNodeEvents,
+    specPattern: "cypress/e2e/features/**/*.feature",
+    chromeWebSecurity: false,
+     
+    },
 });
